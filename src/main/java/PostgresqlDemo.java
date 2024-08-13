b7f8896852957fd8adfd1521144e45e076ef6fee
Sarangantth <sarangantth@gmail.com>
2024-08-13 22:36:54 +0530

    Edited Postgresql file

--- src/main/java/PostgresqlDemo.java
+++ src/main/java/PostgresqlDemo.java
@@ -14,22 +14,24 @@ public class PostgresqlDemo {
         String url = "jdbc:postgresql://localhost:5432/Saran";
         String user = "postgres";
         String password = "Deepikasaran@2305";
-        String sql = "select * from students";
-
+//        String sql = "select * from students"; //Select
+//        String sql = "insert into students values (6, 'Mosa', 20)"; //insert
+//        String sql = "update students set sname = 'Deepak' where sid = 6"; //update
+        String sql = "delete from students where sid =6"; //delete
 //    Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(url, user, password);
         System.out.println("Connections established");
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-//        rs.next();
-//        String name = rs.getString("sname");
-//        System.out.println("Name of the student is " + name);
 
-        while (rs.next()) {
-            System.out.print(rs.getInt(1) + " - ");
-            System.out.print(rs.getString(2) + " - ");
-            System.out.println(rs.getString(3));
-        }
+        st.execute(sql);
+
+//        ResultSet rs = st.executeQuery(sql);
+//        while (rs.next()) {
+//            System.out.print(rs.getInt(1) + " - ");
+//            System.out.print(rs.getString(2) + " - ");
+//            System.out.println(rs.getString(3));
+//        }
+
         conn.close();
         System.out.println("Connection Closed");
     }
